c1db91a80cd47bef69cfa467d9cc2eb57f848611
Talat09 <talat181008312@gmail.com>
2025-01-10 18:58:57 +0600

    Done: Card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,206 @@
 import Image from "next/image";
-
+import cardImage from "../assets/card-image.png";
+import heartImage from "../assets/heart.png";
+import mapIcon from "../assets/location.png";
+import wifi from "../assets/WifiHigh.png";
+import swimmingPool from "../assets/SwimmingPool.png";
+import building from "../assets/Frame.png";
+import starIcon from "../assets/Star.png";
+import viewIcon from "../assets/ArrowSquareOut.png";
+import thumbIcon from "../assets/thumb_up.png";
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="py-3 ">
+      <div className="max-w-[1088px] mx-auto bg-white rounded-xl shadow-xl overflow-hidden">
+        <div className="flex flex-col lg:flex-row">
+          {/* Left side with image */}
+          <div className="flex justify-center items-center mx-auto relative  pt-[14px] ps-[16px] pr-[16px] lg:pr-0  pb-[16px]">
+            <div className="flex justify-center items-center">
+              <Image
+                src={cardImage}
+                alt="Hotel room"
+                className="rounded-[8px] w-full lg:w-[335px] lg:h-[235px] object-cover"
+              />
+            </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+            <div className="absolute w-[32px] h-[32px] top-12 md:top:8 lg:top-10 right-14 md:right-12 lg:right-1 p-2 rounded-full bg-gray-400/75">
+              <Image className=" " src={heartImage} alt="heart" />
+            </div>
+            <div className="w-[59.65px] h-[20.57px] bg-gray-500/90 flex justify-center items-center gap-[6.17px] p-[6px] absolute bottom-10 rounded-[20.57px]">
+              <div className="w-[8.23px] h-[8.23px] bg-white rounded-full"></div>
+              <div className="w-[8.23px] h-[8.23px] bg-[#959595] rounded-full"></div>
+              <div className="w-[8.23px] h-[8.23px] bg-[#959595] rounded-full"></div>
+              <div className="w-[6.17px] h-[6.17px] bg-[#959595] rounded-full"></div>
+            </div>
+          </div>
+
+          <div className="md:flex md:justify-between ">
+            {/* Middle content */}
+            <div className="flex flex-col md:flex-row">
+              <div className="pt-[14px] ps-[24px] pb-[44px] pr-[13px]">
+                <div className="flex items-start">
+                  <div>
+                    <div className="flex items-center gap-4 mb-3 md:mb-4">
+                      <h2 className="text-[16px] md:text-2xl font-bold text-[#000000]">
+                        Hotel Golden Palace, Puri
+                      </h2>
+                      <div className="flex gap-[5px]">
+                        <Image
+                          className="w-4 h-4"
+                          src={starIcon}
+                          alt="star icon"
+                        />
+                        <Image
+                          className="w-4 h-4"
+                          src={starIcon}
+                          alt="star icon"
+                        />
+                        <Image
+                          className="w-4 h-4"
+                          src={starIcon}
+                          alt="star icon"
+                        />
+                        <Image
+                          className="w-4 h-4"
+                          src={starIcon}
+                          alt="star icon"
+                        />
+                      </div>
+                    </div>
+                    <div className="flex items-center mb-3 md:mb-4 gap-[2px]">
+                      <Image
+                        className="w-4 h-4 block lg:hidden"
+                        src={mapIcon}
+                        alt="location"
+                      />
+                      <p className="text-[#626262] ">
+                        VIP Rd, Puri, Odisha-752002
+                      </p>
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col-reverse lg:flex-row lg:items-center gap-4 mb-3 md:mb-4">
+                  <div className="flex items-center gap-2">
+                    <span className="text-xs text-[#5054D9] font-bold">
+                      View property in map
+                    </span>
+                    <Image className="w-4 h-4" src={viewIcon} alt="view icon" />
+                  </div>
+                  <div className="flex items-center">
+                    <span className="text-[#000000]">📍</span>
+                    <span className="text-xs ms-2 text-[#424242] font-medium">
+                      34.32 KM from center
+                    </span>
+                  </div>
+                </div>
+
+                <div className="flex gap-4 mb-3 md:mb-4">
+                  <div className="flex items-center">
+                    <Image className="w-4 h-4 mr-2" src={wifi} alt="wifi" />
+                    <span className="text-[9px] md:text-sm font-medium">
+                      Free Wifi
+                    </span>
+                  </div>
+                  <div className="flex items-center">
+                    <Image
+                      className="w-4 h-4 mr-2"
+                      src={swimmingPool}
+                      alt="pool"
+                    />
+                    <span className="text-[9px] md:text-sm font-medium">
+                      Swimming Pool
+                    </span>
+                  </div>
+                  <div className="flex items-center">
+                    <Image
+                      className="w-4 h-4 mr-2"
+                      src={building}
+                      alt="building"
+                    />
+                    <span className="text-[9px] md:text-sm font-medium">
+                      City View
+                    </span>
+                  </div>
+                </div>
+
+                <div className="flex items-center mb-3 md:mb-4">
+                  <button className="w-[161px] h-[32px] bg-[#E1E1E159] flex justify-center items-center rounded-[9.5px]">
+                    <Image
+                      className="w-4 h-4 mr-2"
+                      src={thumbIcon}
+                      alt="thumb up"
+                    />
+                    <div className="flex gap-2">
+                      <span className="text-base text-[#000000] font-semibold">
+                        4.5{" "}
+                      </span>
+                      <span className="text-[#626262] text-base font-medium">
+                        (23 Reviews)
+                      </span>
+                    </div>
+                  </button>
+                </div>
+              </div>
+
+              <div className="border-dashed border-r-[1px] border-[#626262] hidden md:block"></div>
+              <div className="border-dashed border-b-[1px] border-[#626262] block md:hidden"></div>
+            </div>
+
+            {/* Right side pricing */}
+            <div className="px-4 pt-[21px] pb-[19px] hidden md:flex flex-col justify-between">
+              <div>
+                <div className="flex items-center justify-end gap-4 mb-4">
+                  <button className="w-[54px] h-[26px] text-xs font-bold bg-[#B8BBFF40] rounded-[4px] text-[#5054D9]">
+                    Best
+                  </button>
+                  <button className="w-[89px] h-[26px] text-xs font-bold bg-[#FFD18140] rounded-[4px] text-[#F99F1D]">
+                    Cheapest
+                  </button>
+                </div>
+                <div className="flex items-center justify-end mb-1">
+                  <span className="text-base line-through text-[#444444]">
+                    ₹ 34,440.87
+                  </span>
+                  <span className="ml-2 text-2xl font-bold text-blue-600">
+                    ₹ 31,440.87
+                  </span>
+                </div>
+                <p className="text-[10px] text-[#5A5A5A] text-right mb-2">
+                  Includes Taxes & Charges
+                </p>
+                <p className="text-base text-[#5A5A5A] text-right mb-2">
+                  3 nights
+                </p>
+                <p className="text-sm text-gray-600 text-right mb-4">
+                  2 Adult, 2 Child
+                </p>
+              </div>
+              <div className="flex justify-end">
+                <button className="w-[274px] h-[45px] border border-[#5054D9] text-[#5054D9] rounded-[8px] font-medium text-md">
+                  Choose Room
+                </button>
+              </div>
+            </div>
+            {/* Right side pricing  for mobile devices*/}
+            <div className="p-6 flex items-center gap-2 md:hidden">
+              <div className="flex items-center justify-end mb-1">
+                <p className=" text-xl  text-[#5054D9] font-bold">
+                  ₹ 31,440.87
+                  <span className="text-base  text-[#707070] font-bold">
+                    / 3 nights
+                  </span>
+                </p>
+              </div>
+              <div className="flex">
+                <button className="w-[129px] h-[32px] border border-[#5054D9] text-[#5054D9] rounded-[8px] font-medium text-sm">
+                  Choose Room
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
     </div>
   );
 }
